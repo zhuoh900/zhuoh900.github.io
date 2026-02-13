ef14c5355d16d1d4441f5880d2743a1328f51e0f
zhuoh900 <zhuoh900@newschool.edu>
2026-02-12 19:15:27 -0500

    1

--- entry-8/script.js
+++
@@ -1,9 +0,0 @@
-const slider = document.getElementById("rpmSlider");
-const needle = document.getElementById("needle");
-
-
-slider.addEventListener("input", () => {
-  const rpm = slider.value;
-  const angle = (rpm / 8000) * 240 - 140;
-  needle.style.transform = `rotate(${angle}deg)`;
-});
